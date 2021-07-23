761445a1d070ce9d4012755e1cd74eb921c92fa2
rangamboa <rangamboa@gmail.com>
2021-07-22 23:19:48 -0700

    testing basic links/routes

--- Develop/index.js
+++ Develop/index.js
@@ -3,16 +3,18 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('/routes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/routes.html'))
+  res.sendFile(path.join(__dirname, '/public/routes.html'))
 });
 
 app.listen(port, () => {
