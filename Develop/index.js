1e280dc6b2780925c2bed0fd281601f591886fd9
rangamboa <rangamboa@gmail.com>
2021-07-23 18:36:45 -0700

    JSON troubleshooting

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
+// Set up packages.
 const fs = require('fs');
 const path = require('path');
+
+// Set up express.
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Set up main routes.
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
@@ -16,32 +19,22 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+app.get('/api/notes', (req, res) => {
+
+  // fs.readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  fs.readFile('./db/db.json', 'utf8', (err, data) => { if (err) console.error(err) });
+
+});
+
 app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to save a note.`);
-
-  // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
-
-  const newNote = { title, text }
-    
-    // Convert the data to a string so we can save it
-    const noteString = JSON.stringify(newNote);
-
-    // Write the string to a file
-    fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Note for ${newNote.title} has been written to JSON file`
-          )
-    );
+    let data = JSON.stringify(res);
+    fs.writeFile('db/db.json', data, (err) => {
+    if (err) throw err;
+    return true;
+  });
 });
 
-app.get('/routes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/routes.html'))
-});
-
+// Check and display port.
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`----- App is listening on: http://localhost:${PORT}. -----`);
 });
